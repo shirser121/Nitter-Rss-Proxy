4504fc53f707a53277d1d67188a68e3d0834949d
Michal Serlui <70711723+shirser121@users.noreply.github.com>
2023-10-23 21:16:58 +0300

    Add a security option and improve stuff

--- rss_proxy/core/config.py
+++ rss_proxy/core/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL", "https://status.d420.de/api/v1/instances")
 
@@ -7,3 +10,5 @@ REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
 REDIS_CACHE_DURATION_SECONDS = int(os.getenv("REDIS_CACHE_DURATION_SECONDS", 60 * 2))
 HOSTS_CACHE_DURATION_SECONDS = int(os.getenv("HOSTS_CACHE_DURATION_SECONDS", 60 * 60 * 24))
+
+SECURITY_CODE = os.getenv("SECURITY_CODE", None)
--- rss_proxy/main.py
+++ rss_proxy/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
 from rss_proxy.core.init import startup_event, shutdown_event
 from rss_proxy.api.v1 import rss
+from rss_proxy.core.config import SECURITY_CODE
+from rss_proxy.utils.logger import logger
 
 app = FastAPI()
 
 app.include_router(rss.router, prefix="/api/v1")
+SECURITY_NAME = "Authorization"
 
 
 @app.on_event("startup")
@@ -20,3 +25,24 @@ async def shutdown():
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the proxy server!"}
+
+
+@app.middleware("http")
+async def verify_security_header(request: Request, call_next):
+    if SECURITY_CODE:
+        header_value = request.headers.get(SECURITY_NAME)
+        query_value = request.query_params.get(SECURITY_NAME)
+
+        # If neither header nor query parameter is provided
+        if not header_value and not query_value:
+            logger.warn(f"Missing security code in both headers and query parameters.")
+            return JSONResponse(status_code=400, content={"detail": "Security code is required."})
+
+        # If the provided header or query parameter value doesn't match the expected value
+        if ((header_value and header_value != SECURITY_CODE) or
+                (query_value and query_value != SECURITY_CODE)):
+            logger.warn(f"Invalid security code provided.")
+            return JSONResponse(status_code=400, content={"detail": "Invalid security code."})
+
+    response = await call_next(request)
+    return response
